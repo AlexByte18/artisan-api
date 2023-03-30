7d83132c2fbb095a989652108c8246f849a8f5cb
AlexByte <alex.g.cortes.g@gmail.com>
2023-03-29 23:54:47 -0600

    add seed

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,7 @@
 import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn, BeforeUpdate, OneToMany } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 
-@Entity()
+@Entity({ name: 'products' })
 export class Product {
     @PrimaryGeneratedColumn('uuid')
     id: string
