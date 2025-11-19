4e7b5d26bf1016a83b47e9a0c08ae696e304998d
THAlexCortes <alex.g.cortes.g@gmail.com>
2025-11-19 00:47:10 -0600

    fix(api): fix typo error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,7 @@ export class AuthService {
 
       return {
         ...user,
-        toke: this.getJwtToken({ id: user.id })
+        token: this.getJwtToken({ id: user.id })
       }
 
     } catch (error) {
@@ -57,7 +57,7 @@ export class AuthService {
 
     return {
       ...user,
-      toke: this.getJwtToken({ id: user.id })
+      token: this.getJwtToken({ id: user.id })
     }
   }
 
