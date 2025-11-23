e745cdbd3578bca0af7421e5650d5cdaaf3a6a9f
THAlexCortes <alex.g.cortes.g@gmail.com>
2025-11-22 23:17:45 -0600

    feat: document entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,28 +1,42 @@
 import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn, BeforeUpdate, OneToMany, ManyToOne } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 import { User } from "src/auth/entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name: 'products' })
 export class Product {
+    @ApiProperty({
+      example: '3fa85f64-5717-4562-b3fc-2c963f66',
+      description: 'Product ID',
+      uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string
     
+    @ApiProperty({
+      example: 'T-Shirt',
+      description: 'Product title',
+      uniqueItems: true
+    })
     @Column('text', {
         unique: true
     })
     title: string
 
+    @ApiProperty()
     @Column('numeric', {
         default: 0
     })
     price: number
 
+    @ApiProperty()
     @Column({
         type: 'text',
         nullable: true
     })
     description: string
 
+    @ApiProperty()
     @Column({
         type: 'text',
         unique: true,
@@ -30,25 +44,30 @@ export class Product {
     })
     slug: string
 
+    @ApiProperty()
     @Column('int', {
         default: 0
     })
     stock: Number
 
+    @ApiProperty()
     @Column('text', {
         array: true
     })
     sizes: string[]
 
+    @ApiProperty()
     @Column('text')
     gender: string
     
+    @ApiProperty()
     @Column('text', {
         array: true,
         default: []
     })
     tags: string[]
 
+    @ApiProperty()
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
